c6943ac30cf792439d912da80069f8e5400f71c4
brunobd <bbduarte@live.com>
2023-10-19 01:49:54 -0300

    :feat: add Categories and CategoryItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,18 @@
 "use client"
 import Image from "next/image";
+import Categories from "./components/categories";
 export default function Home() {
   return (
     <div className="p-5">
-      <Image src="/banner-home-01.png" 
-      width={0}
-      height={0}
-      className="h-auto w-full"
-      sizes="100vw"
-      alt="Até 55% desconto só esse mês"/>
+      <Image src="/banner-home-01.png"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% desconto só esse mês" />
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   )
 }
