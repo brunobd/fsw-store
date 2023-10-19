0ad56c87324714fb80d4ed8fd695d5205bd9f11b
brunobd <bbduarte@live.com>
2023-10-19 03:24:01 -0300

    feat: add second banner to home page

--- src/app/(home)/components/products-list.tsx
+++ src/app/(home)/components/products-list.tsx
@@ -8,13 +8,10 @@ interface ProductsListProps {
 
 const ProductsList = ({ products }: ProductsListProps) => {
   return (
-    <div>
-      <h2 className="px-5">Ofertas</h2>
       <div
         className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
         {products.map(product => <ProductItem key={product.id} product={computeProductFinalPrice(product)} />)}
       </div>
-    </div>
 
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,9 @@ import { prismaClient } from "@/lib/prisma";
 export default async function Home() {
 
   const productsOnOffer = await prismaClient.product.findMany({
-    where:{
-      discountPercentage:{
-        gt:0
+    where: {
+      discountPercentage: {
+        gt: 0
       }
     }
   })
@@ -21,12 +21,22 @@ export default async function Home() {
         className="h-auto w-full px-5"
         sizes="100vw"
         alt="Até 55% desconto só esse mês" />
+
       <div className="mt-8 px-5">
         <Categories />
       </div>
+
       <div className="mt-8">
-      <ProductsList products={productsOnOffer}/>
+        <p className="pl-5 font-bold uppercase mb-3">Ofertas</p>
+        <ProductsList products={productsOnOffer} />
       </div>
+
+      <Image src="/banner-home-02.png"
+        width={0}
+        height={0}
+        className="h-auto w-full px-5 mt-8"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses" />
     </div>
   )
 }
