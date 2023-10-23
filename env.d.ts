802c728d4bfcbf6600fe19e2ad48a802c030dfff
brunobd <bbduarte@live.com>
2023-10-23 18:27:20 -0300

    feat: add HOST_URL and NETHAUTH to .env

--- env.d.ts
+++ env.d.ts
@@ -3,8 +3,10 @@ declare namespace NodeJS {
     DATABASE_URL: string,
     GOOGLE_CLIENT_ID: string,
     GOOGLE_CLIENT_SECRET: string,
+    NEXTAUTH_SECRET:string,
     STRIPE_SECRET_KEY: string,
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string,
-    STRIPE_WEBHOOK_SECRET_KEY: string
+    STRIPE_WEBHOOK_SECRET_KEY: string,
+    HOST_URL:string
   }
 }
